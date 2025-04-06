8658d4f234d2525b0dd9026b195cd9799a03d508
ardiansyahgeo <67992886+ardiansyahgeo@users.noreply.github.com>
2025-04-06 16:07:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle panel statistik
+document.getElementById('toggleStats').addEventListener('click', () => {
+  const stats = document.getElementById('stats');
+  stats.style.display = stats.style.display === 'none' ? 'block' : 'none';
+});
